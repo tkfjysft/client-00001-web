abf30ed3897896d5153776d13866a047d7a2a18a
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-05-04 23:25:18 +0900

    feat:トップページ大改修中4

--- src/components/MessageSection.tsx
+++ src/components/MessageSection.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import {
+  Cog,
+  Orbit,
+} from "lucide-react";
 import { Container } from "@/components/Container";
 import ArrowLink from "@/components/ArrowLink";
 import { motion, Variants } from "framer-motion"; // Variantsを追加
@@ -10,7 +14,7 @@ const MESSAGE_CONTENT = {
     catchphrase: {
       top: "テクノロジーで",
       middle_1: "ビジネスの",
-      middle_2: "「次」を",
+      middle_2: "「次」を",	
       bottom: "形にする。",
     },
     paragraphs: [
@@ -26,21 +30,33 @@ const baseDelay = 0.08;
 const layouts = {
   class: {
   headline: "",
-  catchCopy:  "col-start-2  col-end-24  row-start-2  row-end-14   z-20",
-  text:       "col-start-6 col-end-25  row-start-10  row-end-24  z-20",
+  catchCopy:  "col-start-3  col-end-18  row-start-2  row-end-8   z-20",
+  text:       "col-start-8 col-end-18  row-start-8  row-end-24  z-20",
+  text_2:     "col-start-14 col-end-23  row-start-17  row-end-24  z-20",
   bgCircle01: "col-start-15  col-span-25  row-start-7  row-span-20  z-10 opacity-30",
   bgCircle02: "col-start-16 col-end-25  row-start-4  row-end-12  z-0  opacity-20",
-  bgCircle03: "col-start-4  col-end-18  row-start-16 row-end-24  z-10 opacity-40",
-  subText: "",
+  bgCircle03: "col-start-4  col-end-18  row-start-14 row-end-24  z-10 opacity-40",
+  gear01:     "col-start-17  col-span-17  row-start-2  row-span-7  z-10",
+  gear02:     "col-start-20  col-span-20  row-start-6  row-span-11  z-10",
+  gear03:     "col-start-22  col-span-22  row-start-5  row-span-9  z-10",
+  gear04:     "col-start-2  col-span-2  row-start-15  row-span-24  z-10",
+  gear05:     "col-start-4  col-span-4  row-start-17  row-span-24  z-10",
+subText: "",
   },
   delay: {
   headline:   baseDelay * 1,
   catchCopy:  baseDelay * 2,
   text:       baseDelay * 3,
+  text_2:     baseDelay * 3,
   bgCircle01: baseDelay * 4,
   bgCircle02: baseDelay * 5,
   bgCircle03: baseDelay * 6,
   subText:    baseDelay * 7,
+  gear01:    baseDelay * 7,
+  gear02:    baseDelay * 7,
+  gear03:    baseDelay * 7,
+  gear04:    baseDelay * 7,
+  gear05:    baseDelay * 7,
   },
 };
 
@@ -90,7 +106,7 @@ export default function MessageSection({
       <Container>
 
           {/* セクション見出し装飾 */}
-		  <FadeIn className="flex items-center gap-4 mb-40" delay={layouts.delay.headline}>
+		  <FadeIn className="flex items-center gap-4 mb-10" delay={layouts.delay.headline}>
             <div className="w-4 sm:w-12 h-[2px] bg-clr-primary-1" />
             <span className="text-clr-base-1 font-mono text-xs tracking-[0.1em] sm:tracking-[0.4em] uppercase">
               {MESSAGE_CONTENT.mission}
@@ -100,16 +116,16 @@ export default function MessageSection({
 
 
 			<div className="grid grid-cols-[repeat(24,1fr)] grid-rows-[repeat(24,1fr)]
-			max-h-[100vh] min-h-[70vh] w-full gap-0 min-h-screen
+			 w-full gap-0 min-h-screen
 			 relative overflow-hidden">
 
 				{/* キャッチコピー（左側） */}
 				<FadeIn className={`
 						${layouts.class.catchCopy}`} delay={layouts.delay.catchCopy}>
 							<h2 className="text-3xl md:text-5xl font-[700] text-clr-base-1/90 !leading-[1.5]">
-								{MESSAGE_CONTENT.catchphrase.top}<br />
-								{MESSAGE_CONTENT.catchphrase.middle_1}
-								{MESSAGE_CONTENT.catchphrase.middle_2}<br />
+								{MESSAGE_CONTENT.catchphrase.top}<br className="block lg:hidden" />
+								{MESSAGE_CONTENT.catchphrase.middle_1}<br />
+								{MESSAGE_CONTENT.catchphrase.middle_2}
 								<span className="text-clr-primary-1">{MESSAGE_CONTENT.catchphrase.bottom}</span>
 							</h2>
 							{/* <div className="absolute size-full inset-0 bg-clr-main-2 */}
@@ -119,24 +135,54 @@ export default function MessageSection({
 
 
 				{/* 本文（右側） */}
-				<FadeIn className={`
+				<FadeIn className={`relative pt-20 border-t-[0.1px] border-clr-base-1/30
+									after:content-['']
+									after:absolute
+									after:top-[-4.5px]
+									after:-left-2
+									after:size-[8px]
+									after:bg-clr-base-1/30
+									after:rounded-full
 				${layouts.class.text}`} delay={layouts.delay.text}>
 							<div className="space-y-6">
-								{MESSAGE_CONTENT.paragraphs.map((text, i) => (
-								<p key={i} className={i === 0 
-									? "text-clr-base-1/80 text-xl font-medium md:text-2xl" 
-									: "text-clr-base-1/80 text-lg"
+								{/* {MESSAGE_CONTENT.paragraphs.map((text, i) => ( */}
+								{/* <p key={0} className={ */}
+								<p className={
+									"text-clr-base-1/80 text-xl font-medium md:text-2xl" 
 								}>
-									{text}
+									{MESSAGE_CONTENT.paragraphs[0]}
 								</p>
-								))}
-								<ArrowLink href={MESSAGE_CONTENT.link || "#"} />
+								{/* ))} */}
 							</div>
 
 				</FadeIn>
 
+				<FadeIn className={`relative pt-20 border-t-[0.1px] border-clr-base-1/30
+									after:content-['']
+									after:absolute
+									after:top-[-4.5px]
+									after:-left-2
+									after:size-[8px]
+									after:bg-clr-base-1/30
+									after:rounded-full
+				${layouts.class.text_2}`} delay={layouts.delay.text_2}>
+							<div className="space-y-6">
+								{/* {MESSAGE_CONTENT.paragraphs.map((text, i) => ( */}
+								{/* <p key={1} className={ */}
+								<p className={
+									"text-clr-base-1/80 text-lg"
+								}>
+									{MESSAGE_CONTENT.paragraphs[1]}
+								</p>
+								{/* ))} */}
+								<ArrowLink variant="white" href={MESSAGE_CONTENT.link  || "#"} className="mt-20"/>
+							</div>
+
+				</FadeIn>
+
+
 				{/* 装飾図形（同じセル、あるいは重ねる配置） */}
-				<FadeIn className={`
+				{/* <FadeIn className={`
 				${layouts.class.bgCircle01}`} delay={layouts.delay.bgCircle01}>
 					<div className="size-15 bg-clr-primary-3/[0.3] border border-clr-base-1/10 backdrop-saturate-150 shadow-xl" />
 				</FadeIn>
@@ -149,12 +195,47 @@ export default function MessageSection({
 				<FadeIn className={`
 				${layouts.class.bgCircle03}`} delay={layouts.delay.bgCircle03}>
 					<div className="size-40 bg-clr-primary-3/[0.3] border border-clr-base-1/10 backdrop-saturate-150 shadow-xl" />
-				</FadeIn>
+				</FadeIn> */}
 
+				<FadeIn className={layouts.class.gear01} delay={layouts.delay.gear01}>
+					<Cog 
+					strokeWidth={1.5}
+					size={200} 
+					className="text-clr-main-2-2 animate-spin-slow" 
+					/>
+				</FadeIn>
+				<FadeIn className={layouts.class.gear02} delay={layouts.delay.gear02}>
+					<Cog 
+					size={120} 
+					strokeWidth={1.5}
+					className="text-clr-main-2-2 animate-spin-reverse" 
+					/>
+				</FadeIn>
+				<FadeIn className={layouts.class.gear03} delay={layouts.delay.gear03}>
+					<Cog 
+					size={80} 
+					strokeWidth={1.5}
+					className="text-clr-main-2-2 animate-spin-slow" 
+					/>
+				</FadeIn>
+				<FadeIn className={layouts.class.gear04} delay={layouts.delay.gear04}>
+					<Cog 
+					size={180} 
+					strokeWidth={1.8}
+					className="text-clr-main-2-2 animate-spin-reverse-moreslow" 
+					/>
+				</FadeIn>
+				<FadeIn className={layouts.class.gear05} delay={layouts.delay.gear05}>
+					<Cog 
+					size={340} 
+					strokeWidth={1.2}
+					className="text-clr-main-2-2 animate-spin-moreslow" 
+					/>
+				</FadeIn>
 			</div>
 
 			{/* 下部の署名 */}
-			<FadeIn className="flex justify-between items-center gap-4 mt-30" delay={layouts.delay.subText}>
+			<FadeIn className="flex justify-between items-center gap-4 mt-20" delay={layouts.delay.subText}>
                 <div className="flex-grow h-[1px] bg-clr-primary-1/30 reveal" />
                 <span className="text-clr-base-1 font-mono text-xs italic">
                   {MESSAGE_CONTENT.signature}
